0181bc8663773f86f3af0b3d21aeb85c939fd5d1
Kevin <darmawankevinsanadi@gmail.com>
2023-04-16 16:44:33 +0700

    added person class

--- src/Hotel/Room.java
+++ src/Hotel/Room.java
@@ -1,5 +1,26 @@
 package Hotel;
 
 public class Room {
-
+	private boolean availability;
+	private boolean clean_status;
+	private String type;
+	private Fridge fridge;
+	private Person guest;
+	
+	public Room(String type, Fridge fridge) {
+		availability = true;
+		clean_status = true;
+		this.type = type;
+		this.fridge = fridge;
+		guest = null;
+	}
+	
+	public void checkIn(Person guest) {
+		availability = false;
+		this.guest = guest;
+	}
+	public void checkOut() {
+		availability = true;
+		this.guest = null;
+	}
 }
