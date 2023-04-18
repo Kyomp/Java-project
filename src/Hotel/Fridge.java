dc75564cd2e07e9dcd253b13b75fe3a195ce1879
Kevin <darmawankevinsanadi@gmail.com>
2023-04-18 13:19:46 +0700

    removed floor and added methods and fields for Fridge, Person, and Room

--- src/Hotel/Fridge.java
+++ src/Hotel/Fridge.java
@@ -1,5 +1,40 @@
 package Hotel;
 
-public class Fridge {
+import java.util.HashMap;
+import java.util.List;
 
+public class Fridge {
+	private HashMap<String,Integer> contents;
+	private static HashMap<String,Integer> validItems = new HashMap<String,Integer>();
+	
+	public Fridge() {
+		contents = new HashMap<String,Integer>();
+	}
+	public Fridge(List<String> contents) {
+		for(String item : contents) {
+			if(!validItems.containsKey(item)) {
+				throw new IllegalArgumentException(item + " is not a valid item to put in the fridge");
+			}
+			if(this.contents.containsKey(item)) {
+				this.contents.put(item, this.contents.get(item)+1);
+			}
+			else {
+				this.contents.put(item, 1);
+			}
+		}
+	}
+	
+	public void removeItem() {
+		
+	}
+	public static void addAllValidItems(HashMap<String,Integer> validItems) {
+		Fridge.validItems.putAll(validItems);
+	}
+	public static void addValidItem(String name, Integer price) {
+		Fridge.validItems.put(name,price);
+	}
+	public static void addValidItem(String name, int price) {
+		Fridge.validItems.put(name, Integer.valueOf(price));
+	}
+	
 }
--- src/Hotel/Room.java
+++ src/Hotel/Room.java
@@ -1,26 +1,44 @@
 package Hotel;
 
+import java.util.List;
+
 public class Room {
 	private boolean availability;
 	private boolean clean_status;
 	private String type;
 	private Fridge fridge;
 	private Person guest;
+	private final int roomNumber;
+	private int costPerNight;
 	
-	public Room(String type, Fridge fridge) {
+	public Room(int roomNumber, String type, int costPerNight) {
 		availability = true;
 		clean_status = true;
+		this.roomNumber = roomNumber;
 		this.type = type;
-		this.fridge = fridge;
+		this.fridge = new Fridge();
+		this.costPerNight = costPerNight;
+		guest = null;
+	}
+	
+	public Room(int roomNumber, List<String> fridgeContents,String type, int costPerNight) {
+		availability = true;
+		clean_status = true;
+		this.roomNumber = roomNumber;
+		this.type = type;
+		this.fridge = new Fridge(fridgeContents);
+		this.costPerNight = costPerNight;
 		guest = null;
 	}
 	
 	public void checkIn(Person guest) {
 		availability = false;
+		guest.checkIn(roomNumber);
 		this.guest = guest;
 	}
 	public void checkOut() {
 		availability = true;
+		guest.checkOut();
 		this.guest = null;
 	}
 }
