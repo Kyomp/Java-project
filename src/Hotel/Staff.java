a47191dd5c0154802e1fd944209708c5d447c245
Deciano <Deciano@DecianoPG15>
2023-05-23 18:05:36 +0700

    Added attributes and methods to "Staff" class

--- src/Hotel/Staff.java
+++ src/Hotel/Staff.java
@@ -1,5 +1,35 @@
 package Hotel;
 
 public class Staff {
-
+	private final String staffID;
+	private final String staffName;
+	private Room work_room;
+	
+	public Staff(String staffID, String staffName) {
+		if(staffID.matches("^(F|M).*\\[0-9]{3}$")) {
+			this.staffID = staffID;
+		} else {
+			throw new IllegalArgumentException("The ID input has incorrect format");
+		}
+		this.staffName = staffName;
+		this.work_room = null;
+	}
+	
+	public Staff(String staffID, String staffName, Room work_room) {
+		if(staffID.matches("^(F|M).*\\[0-9]{3}$")) {
+			this.staffID = staffID;
+		} else {
+			throw new IllegalArgumentException("The ID input has incorrect format");
+		}
+		this.staffName = staffName;
+		this.work_room = work_room;
+	}
+	
+	public void assignRoom(Room room) {
+		this.work_room = room;
+	}
+	
+	public void deassignRoom() {
+		this.work_room = null;
+	}
 }
