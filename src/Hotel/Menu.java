7313c9d7ec272df4a38968c21a4b0abe4ec27f0f
Kevin <darmawankevinsanadi@gmail.com>
2023-05-23 19:16:58 +0700

    added a bit

--- src/Hotel/Menu.java
+++ src/Hotel/Menu.java
@@ -1,9 +1,17 @@
 package Hotel;
 
-public class Menu {
+import java.util.ArrayList;
 
+public class Menu {
+	private ArrayList<Room> hotelRooms;
 	public Menu() {
+		hotelRooms = new ArrayList<Room>();
+		
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void addRooms() {
+		
 	}
 	public static void main(String[] args) {
 		new Menu();
